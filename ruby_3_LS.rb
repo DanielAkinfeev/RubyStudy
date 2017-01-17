675a901ee1a698faba294516ed6fd282047fafbc
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-17 18:12:29 +0300

    Add files via upload

---
+++ ruby_3_LS.rb
@@ -0,0 +1,7 @@
+last_name = "Bond"
+puts  "My name is " + last_name
+puts "James " + last_name
+
+last_name = "Khayriyyah karim al-Rashid"
+puts  "My name is " + last_name
+puts "James " + last_name
