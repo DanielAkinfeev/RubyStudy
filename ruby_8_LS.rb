fae521c91fee32efb0a50cd2bcad1228353c3221
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-18 12:14:34 +0300

    Add files via upload

---
+++ ruby_8_LS.rb
@@ -0,0 +1,17 @@
+a = 1
+while a < 10
+	a+= 1
+	puts "'a' variable is " + a.to_s
+end
+
+a=if ARGV[0]
+	ARGV[0].to_i
+else
+	a = 1
+end
+
+while a < 10
+	a+= 1
+	puts "'a' variable is " + a.to_s
+	sleep 1
+end
