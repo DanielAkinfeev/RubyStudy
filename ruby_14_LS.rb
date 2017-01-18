0b2be7d390ba53141bba3987713eba9129a6e67a
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-18 13:45:21 +0300

    Add files via upload

---
+++ ruby_14_LS.rb
@@ -0,0 +1,20 @@
+class Item
+
+	def initialize
+		@price = 30
+	end
+
+	#attr_reader :price, :weight
+	#attr_writer :price, :weight
+	attr_accessor :price, :weight
+
+end
+
+item1= Item.new
+item1.price = 10
+puts item1.price
+
+item1.weight=10
+puts item1.weight
+
+
