d2444609e62e35b6e77886dae2869d53c2f9ccf7
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-18 11:57:40 +0300

    Add files via upload

---
+++ ruby_7_LS.rb
@@ -0,0 +1,42 @@
+if 2 > 1
+	 puts	"2 is greater then 1"
+end
+
+i = 0
+
+if i > 1
+	 puts "number is greater than 1"
+end
+
+if i < 1
+	puts "number is less than 1"
+end
+
+
+
+if i > 1
+	puts "number is greater than 1"
+elsif i < 1
+	puts "number is less than 1"
+end
+
+
+i = 1
+
+if i >= 1
+	puts "number is greater than or equals 1"
+elsif i <= 1
+	puts "number is less than or equals 1"
+else
+	puts "number equals 1"
+end
+
+puts "---------------------"
+
+if  i >= 1
+	puts "number is greater than or equals 1"
+end
+
+if i <= 1
+	puts "number is less than  or equals1"
+end
