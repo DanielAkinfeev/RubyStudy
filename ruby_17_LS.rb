a8cbf4e347da19e302dbe56fb15134c31713819a
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-18 15:57:36 +0300

    Add files via upload

---
+++ ruby_17_LS.rb
@@ -0,0 +1,17 @@
+names = ["Bob", "James", "John"]
+
+p names
+puts names[0]
+puts names[1]
+puts names[2]
+
+puts names.class
+names.push "Daniel"
+p names
+
+names.pop
+p names
+
+names.shift
+p names
+puts names[0]
