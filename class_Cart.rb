11e4fabd4353417847771e1bc56bb44c9d98af48
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-19 14:04:42 +0300

    Add files via upload

--- class_Cart.rb
+++ class_Cart.rb
@@ -1,14 +1,26 @@
 class Cart
 	
 	attr_reader :items
-	def initialize
+
+	include ItemContainer
+
+	def initialize(owner)
 		@items = Array.new
+		@owner = owner
 	end
-	def add_item(item)
-		@items.push item
+	
+	def save_to_file
+		File.open("#{@owner}_cart.txt", "w") do  |f|
+			@items.each { |i| f.puts i } #car:100:50
+		end 
 	end
 
-	def remove_item
-		@items.pop
+	def read_from_file
+			#return unless File.exists?("#{@owner}_cart.txt")
+			File.readlines("#{@owner}_cart.txt").each { |i| @items << i.to_real_item}
+			@items.uniq!
+		rescue Errno::ENOENT
+			File.open("{@owner}_cart.txt", "w") {}
+			puts "file #{@owner}_cart.txt created"
 	end
 end
