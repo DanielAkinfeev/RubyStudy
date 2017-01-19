78b1e43536d6423546c7d24ffb0ae399fe7c8e64
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-19 13:02:29 +0300

    Add files via upload

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
+require_relative "item_container.rb"
+require_relative "order.rb"
 require_relative "class_Cart"
 require_relative "class_item"
+require_relative "virtual_item.rb"
+require_relative "real_item.rb"
 
-cart = Cart.new
+@items =[]
+@items << VirtualItem.new({:price => 101, :weight => 100, :name => "car"})
+@items << RealItem.new({:weight => 100, :price => 101, :name => "kettle"})
+@items << RealItem.new({:weight => 100, :price => 101, :name => "dishwasher"})
 
-cart.add_item(Item.new)
-cart.add_item(Item.new)
-p cart.items
-
-cart.remove_item
-
-p cart.items
