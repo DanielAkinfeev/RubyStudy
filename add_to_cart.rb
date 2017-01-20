dbf33d1bc1e2f5ff9a93195f9e5cc5019c4c99d3
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-20 11:35:30 +0300

    Add files via upload

--- add_to_cart.rb
+++ add_to_cart.rb
@@ -6,9 +6,9 @@ ARGV.each do |a|
 end
 	cart.read_from_file
 begin
-	
+
 	cart.save_to_file
 
 rescue Cart::ItemNotSupported
-	puts "One of your items is virtual, Cart doesnt support saving virtual items yet" 
+	puts "One of the items you're trying to save is not supported by the Cart. Unsupported item classes are: #{Cart::UNSUPPORTED_ITEMS}" 
 end
--- init.rb
+++ init.rb
@@ -1,14 +1,17 @@
 require_relative "string.rb"
 require_relative "item_container.rb"
-require_relative "order.rb"
-require_relative "class_Cart"
 require_relative "class_item"
 require_relative "virtual_item.rb"
 require_relative "real_item.rb"
+require_relative "antique_item.rb"
+require_relative "class_Cart"
+require_relative "order.rb"
+
+
 
 
 @items =[]
-@items << VirtualItem.new({:price => 101, :weight => 100, :name => "car"})
+@items << AntiqueItem.new({:price => 101, :weight => 100, :name => "car"})
 @items << RealItem.new({:weight => 100, :price => 101, :name => "kettle"})
 @items << RealItem.new({:weight => 100, :price => 101, :name => "dishwasher"})
 
