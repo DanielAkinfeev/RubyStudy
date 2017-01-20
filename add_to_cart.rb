b2199f6b31bc7f524bd95f084285e5e43f1d574e
DanielAkinfeev <deakinfeev@gmail.com>
2017-01-20 11:21:12 +0300

    Add files via upload

--- add_to_cart.rb
+++ add_to_cart.rb
@@ -1,8 +1,14 @@
 require_relative "init.rb"
 
-cart= Cart.new
+cart= Cart.new(ARGV.delete_at(0))
 ARGV.each do |a|
 	@items.each { |i| cart.add_item(i) if a== i.name}
 end
+	cart.read_from_file
+begin
+	
+	cart.save_to_file
 
-p cart.items
+rescue Cart::ItemNotSupported
+	puts "One of your items is virtual, Cart doesnt support saving virtual items yet" 
+end
